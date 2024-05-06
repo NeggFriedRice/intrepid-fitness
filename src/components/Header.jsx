3d2bd5a6acf0f776f4bb8fb3e6f0e2c48bbd41a0
Thomas Loo <tomloo222@gmail.com>
2024-05-07 02:02:11 +1000

    Add reveal animation to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Reveal from '../utils/Reveal'
 
 export default function Header() {
   return (
@@ -7,16 +8,22 @@ export default function Header() {
         <div className="flex xl:w-[1280px] flex-wrap justify-center">
             <div className="w-[100vw] lg:w-[894px] h-[50vh] xl:w-[640px] xl:h-[650px] bg-setColour1 flex place-items-center justify-center">
                 <div className="font-[BuiltTitling] relative">
-                    <div className="absolute rotate-[270deg] top-[55px] left-[-80px]">
-                        <p className="text-[2rem]">IT'S TIME TO</p>
+                    <div className="absolute rotate-[270deg] top-[80px] left-[-145px]">
+                        <p className="text-[3.5rem]">IT'S TIME TO</p>
                     </div>
                     <div className="flex justify-center">
                         <div className="text-left">
-                            <p className="text-[4rem] mb-[-30px]">WORK HARD.</p>
-                            <p className="text-[4.1rem]">TRAIN HARD.</p>
+                            <Reveal direction={"left"} delay={0.35}>
+                                <p className="text-[4rem] mb-[-20px]">WORK HARD.</p>
+                            </Reveal>
+                            <Reveal direction={"right"} delay={0.65}>
+                                <p className="text-[4.1rem] mb-[-20px]">TRAIN HARD.</p>
+                            </Reveal>
                         </div>
                     </div>
-                    <p className="text-[4.5rem] m-[-40px]">SWEAT HARD.</p>
+                    <Reveal direction={"left"} delay={0.95}>
+                        <p className="text-[4.5rem] mt-[-10px]">SWEAT HARD.</p>
+                    </Reveal>
                 </div>
             </div>
             <div className="xl:w-[640px] flex justify-center">
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default function Intro() {
   return (
     <div className="flex justify-center py-8 bg-black font-[BuiltTitling] text-[2rem] text-center">
