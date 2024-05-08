3a632cd1d0f084bcd8a3d2b79e93184a46f72c0e
Thomas Loo <tomloo222@gmail.com>
2024-05-08 12:53:38 +1000

    Add numbers and Staatlich font to classes

--- src/components/Classes.jsx
+++ src/components/Classes.jsx
@@ -11,33 +11,33 @@ export default function Classes() {
             <div className="max-md:w-[600px] max-lg:w-[800px] xl:w-[1280px] relative xl:flex xl:flex-wrap justify-center text-[1.5rem]">
                 <div className="flex flex-wrap justify-center place-items-center my-8">
                     <div className="w-[300px]">
-                        <h1 className="font-[BuiltTitling] text-[3rem]">Boxing</h1>
+                        <h1 className="font-[BuiltTitling] text-[3rem]"><span className="font-[Staatliches] text-[2.5rem]">01. </span>Boxing</h1>
                         <p className="font-[BuiltTitling] text-[2rem]">Prepare to sweat</p>
-                        <p>A high intensity class with a combination of boxing drills along with body weight and core exercises! All levels welcome!</p>
+                        <p className="font-[Lato]">A high intensity class with a combination of boxing drills along with body weight and core exercises! All levels welcome!</p>
                     </div>
                     <div className="w-[300px] mx-4 h-[250px] bg-gray-300"></div>
                 </div>
                 <div className="flex flex-wrap justify-center place-items-center my-8">
                     <div className="w-[300px]">
-                        <h1 className="font-[BuiltTitling] text-[3rem]">Bootcamp</h1>
+                        <h1 className="font-[BuiltTitling] text-[3rem]"><span className="font-[Staatliches] text-[2.5rem]">02. </span>Bootcamp</h1>
                         <p className="font-[BuiltTitling] text-[2rem]">All Rounder</p>
-                        <p>A workout that focuses on all areas of fitness: strength, explosive power, speed, endurance, mental toughness and balance</p>
+                        <p className="font-[Lato]">A workout that focuses on all areas of fitness: strength, explosive power, speed, endurance, mental toughness and balance</p>
                     </div>
                     <div className="w-[300px] mx-4 h-[250px] bg-gray-300"></div>
                 </div>
                 <div className="flex flex-wrap justify-center place-items-center my-8">
                     <div className="w-[300px]">
-                        <h1 className="font-[BuiltTitling] text-[3rem]">Stretch & Core</h1>
+                        <h1 className="font-[BuiltTitling] text-[3rem]"><span className="font-[Staatliches] text-[2.5rem]">03. </span>Stretch & Core</h1>
                         <p className="font-[BuiltTitling] text-[2rem]">Stabilise and Strengthen</p>
-                        <p>Focus on core strength, stabilise the pelvis and increases your mobility and fexibility. Great for injury prevention or recovery.</p>
+                        <p className="font-[Lato]">Focus on core strength, stabilise the pelvis and increases your mobility and fexibility. Great for injury prevention or recovery.</p>
                     </div>
                     <div className="w-[300px] mx-4 h-[250px] bg-gray-300"></div>
                 </div>
                 <div className="flex flex-wrap justify-center place-items-center my-8">
                     <div className="w-[300px]">
-                        <h1 className="font-[BuiltTitling] text-[3rem]">Yoga</h1>
+                        <h1 className="font-[BuiltTitling] text-[3rem]"><span className="font-[Staatliches] text-[2.5rem]">04. </span>Yoga</h1>
                         <p className="font-[BuiltTitling] text-[2rem]">Meditate & Relax</p>
-                        <p>A perfect class to ease back into training with focus on correct movement and technique of stretches and exercises to strengthen the core.</p>
+                        <p className="font-[Lato]">A perfect class to ease back into training with focus on correct movement and technique of stretches and exercises to strengthen the core.</p>
                     </div>
                     <div className="w-[300px] mx-4 h-[250px] bg-gray-300"></div>
                 </div>
--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -9,72 +9,72 @@ export default function Membership() {
         <div className="flex justify-center bg-black pb-4">
             <div className="lg:flex lg:flex-wrap lg:justify-between lg:w-[1000px] lg:py-4">
                 <div className="gold-box bg-white text-black w-[70vw] lg:w-[450px] rounded-lg text-center my-12 lg:my-4 py-4 mx-4 border-4 border-yellow-400 font-[BuiltTitling]">
-                    <h3 className="text-[1.5rem] lg:text-[2.5rem]">Gold Membership</h3>
+                    <h3 className="text-[1.5rem] lg:text-[2rem]">Gold Membership</h3>
                     <p className="text-[2rem] lg:text-[3rem]">$47 per week</p>
-                    <p className="text-[1.25rem] lg:text-[2.25rem]">$88 PW for couples</p>
+                    <p className="text-[1.25rem] lg:text-[2rem]">$88 PW for couples</p>
                     <div className="flex justify-center py-4">
                         <div className="h-[1px] w-[60%] bg-yellow-300"></div>
                     </div>
-                    <ul className="text-[1.5rem]">
-                        <li>Unlimited group classes</li>
-                        <li>Open gym access</li>
-                        <li>Sauna access</li>
-                        <li>Recovery lounge access</li>
-                        <li>Coffee & breakfast</li>
-                        <li>Nutrition app access</li>
+                    <ul className="text-[1.5rem] font-[Lato]">
+                        <li><span className="text-red-500">+</span> Unlimited group classes</li>
+                        <li><span className="text-red-500">+</span> Open gym access</li>
+                        <li><span className="text-red-500">+</span> Sauna access</li>
+                        <li><span className="text-red-500">+</span> Recovery lounge access</li>
+                        <li><span className="text-red-500">+</span> Coffee & breakfast</li>
+                        <li><span className="text-red-500">+</span> Nutrition app access</li>
                     </ul>
                 </div>
                 <div className="silver-box bg-white text-black w-[70vw]  lg:w-[450px] rounded-lg text-center my-12 lg:my-4 py-4 mx-4 border-4 border-gray-300 font-[BuiltTitling]">
-                    <h3 className="text-[1.5rem] lg:text-[2.5rem]">Silver Membership</h3>
+                    <h3 className="text-[1.5rem] lg:text-[2rem]">Silver Membership</h3>
                     <p className="text-[2rem] lg:text-[3rem]">$37 per week</p>
                     <div className="flex justify-center py-4">
                         <div className="h-[1px] w-[60%] bg-gray-300"></div>
                     </div>
-                    <ul className="text-[1.5rem]">
-                        <li>2 group classes per week (incl. yoga)</li>
-                        <li>Sauna access</li>
-                        <li>Recovery lounge access</li>
-                        <li>Coffee & breakfast</li>
-                        <li>Nutrition app access</li>
+                    <ul className="text-[1.5rem] font-[Lato]">
+                        <li><span className="text-red-500">+</span> 2 group classes per week (incl. yoga)</li>
+                        <li><span className="text-red-500">+</span> Sauna access</li>
+                        <li><span className="text-red-500">+</span> Recovery lounge access</li>
+                        <li><span className="text-red-500">+</span> Coffee & breakfast</li>
+                        <li><span className="text-red-500">+</span> Nutrition app access</li>
                     </ul>
                 </div>
                 <div className="sport-box bg-white text-black w-[70vw]  lg:w-[450px] rounded-lg text-center my-12 lg:my-4 py-4 mx-4 border-4 border-red-600 font-[BuiltTitling]">
-                    <h3 className="text-[1.5rem] lg:text-[2.5rem]">Sporting Groups</h3>
+                    <h3 className="text-[1.5rem] lg:text-[2rem]">Sporting Groups</h3>
                     <p className="text-[2rem] lg:text-[3rem]">$10 per session</p>
                     <div className="flex justify-center py-4">
                         <div className="h-[1px] w-[60%] bg-red-300"></div>
                     </div>
-                    <ul className="text-[1.5rem]">
-                        <li>45 minute sessions</li>
-                        <li>Healthy snacks included</li>
+                    <ul className="text-[1.5rem] font-[Lato]">
+                        <li><span className="text-red-500">+</span> 45 minute sessions</li>
+                        <li><span className="text-red-500">+</span> Healthy snacks included</li>
                     </ul>
                 </div>
                 <div className="junior-box bg-white text-black w-[70vw]  lg:w-[450px] rounded-lg text-center my-12 lg:my-4 py-4 mx-4 border-4 border-green-300 font-[BuiltTitling]">
-                    <h3 className="text-[1.5rem] lg:text-[2.5rem]">Juniors Strength Academy</h3>
+                    <h3 className="text-[1.5rem] lg:text-[2rem]">Juniors Strength Academy</h3>
                     <p className="text-[2rem] lg:text-[3rem]">$37 per week</p>
                     <div className="flex justify-center py-4">
                         <div className="h-[1px] w-[60%] bg-green-300"></div>
                     </div>
-                    <ul className="text-[1.5rem]">
-                        <li>12 - 18 year old</li>
-                        <li>Unlimited group classes</li>
-                        <li>Open gym access</li>
-                        <li>Recovery lounge access</li>
-                        <li>Breakfast</li>
-                        <li>Nutrition app access</li>
+                    <ul className="text-[1.5rem] font-[Lato]">
+                        <li><span className="text-red-500">+</span> 12 - 18 year old</li>
+                        <li><span className="text-red-500">+</span> Unlimited group classes</li>
+                        <li><span className="text-red-500">+</span> Open gym access</li>
+                        <li><span className="text-red-500">+</span> Recovery lounge access</li>
+                        <li><span className="text-red-500">+</span> Breakfast</li>
+                        <li><span className="text-red-500">+</span> Nutrition app access</li>
                     </ul>
                 </div>
                 <div className="recovery-box bg-white text-black w-[70vw]  lg:w-[450px] rounded-lg text-center my-12 lg:my-4 py-4 mx-4 border-4 border-violet-400 font-[BuiltTitling]">
-                    <h3 className="text-[1.5rem] lg:text-[2.5rem]">Recovery Lounge Membership</h3>
+                    <h3 className="text-[1.5rem] lg:text-[2rem]">Recovery Lounge Membership</h3>
                     <p className="text-[2rem] lg:text-[3rem]">$37 per week</p>
                     <div className="flex justify-center py-4">
                         <div className="h-[1px] w-[60%] bg-violet-400"></div>
                     </div>
-                    <ul className="text-[1.5rem]">
-                        <li>Unlimited Recovery lounge access</li>
-                        <li>Unlimited sauna access</li>
-                        <li>Unlimited compression boots use</li>
-                        <li>Unlimited foot massager use</li>
+                    <ul className="text-[1.5rem] font-[Lato]">
+                        <li><span className="text-red-500">+</span> Unlimited Recovery lounge access</li>
+                        <li><span className="text-red-500">+</span> Unlimited sauna access</li>
+                        <li><span className="text-red-500">+</span> Unlimited compression boots use</li>
+                        <li><span className="text-red-500">+</span> Unlimited foot massager use</li>
                     </ul>
                 </div>
             </div>
