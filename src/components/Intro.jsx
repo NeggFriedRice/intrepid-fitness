e35ed34f2cddcccc222235a027d8d0ddf2cdf0fe
Thomas Loo <tomloo222@gmail.com>
2024-05-30 15:21:30 +1000

    Add Google maps link to address in intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,8 +5,10 @@ export default function Intro() {
   return (
     <div className="flex justify-center py-8 bg-black font-[BuiltTitling] text-[2rem] text-center">
         <div className="text-[2rem] lg:text-[3.5rem] relative">
+          <a href="https://maps.app.goo.gl/HKpvKUUwu7CZxGcS9">
             <p>2/107-113 Heatherdale Road</p>
             <p>Ringwood</p>
+          </a>
             <div className="h-[2.25rem] lg:h-[4rem] ">
               <p className="hover:text-[2.25rem] hover:text-setColour1 hover:lg:text-[4rem] relative transition-all duration-300 delay-50">
                 <a href="tel:0407853346" className="absolute ml-auto mr-auto left-0 right-0">0407 853 346</a>
